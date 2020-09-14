8102c030d5a8f4efa84098d33dc4e4ca3bec8691
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2020-09-14 23:05:41 +0900

    Remove unused vars

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,10 +5,9 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm, scale } from "../utils/typography"
 
-const BlogPostTemplate = ({ data, pageContext, location }) => {
+const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
   const siteTitle = data.site.siteMetadata.title
-  const { previous, next } = pageContext
 
   return (
     <Layout location={location} title={siteTitle}>
