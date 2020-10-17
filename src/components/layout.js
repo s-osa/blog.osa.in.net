9162655b1fc67daefd78725d74eecd99d62e5fcb
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2020-10-18 05:32:49 +0900

    Underline links

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import "./common.scss"
 import { rhythm, scale } from "../utils/typography"
 
 const Layout = ({ location, title, children }) => {
