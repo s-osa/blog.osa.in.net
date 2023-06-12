08b5828703866e37a09856a35cc18019789b171e
Shunsuke Osa <hhelibebcnofnenamg@gmail.com>
2023-06-13 00:25:19 +0900

    Configure gatsby-plugin-google-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,6 +62,15 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-9XFNY0KVVK", // Google Analytics
+        ],
+      },
+    },
+    {
       resolve: `gatsby-plugin-feed`,
       options: {
         query: `
